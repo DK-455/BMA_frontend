ef759e1ec362646a562bda37bcd13d9d6d0b20a8
Deepuprajapat <153892296+Deepuprajapat@users.noreply.github.com>
2025-04-24 17:42:47 +0530

    Add files via upload

---
+++ src/components/crm/EmployeePerformance.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmployeePerformance = () => {
+    return (
+        <div>
+            Employee Performance
+        </div>
+    )
+}
+
+export default EmployeePerformance
